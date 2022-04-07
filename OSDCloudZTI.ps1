66a5c5954604b11dc1f86ca16e71ab1e495b4d6b
Iain Borghoff <38186303+iborghoff@users.noreply.github.com>
2022-04-07 16:37:58 +0200

    Update OSDCloudZTI.ps1

--- OSDCloudZTI.ps1
+++ OSDCloudZTI.ps1
@@ -3,32 +3,41 @@ using namespace System.Management.Automation.Host
 $scriptverion = '1.0'
 Write-Host -ForegroundColor Yellow "Script version $scriptverion"
 
-$title = ''
-$question = 'Please choose a deployment option:'
-$zti = [ChoiceDescription]::new('&ZTI', 'Zero Touch Install - This is fully automated without any prompts.')
-$gui = [ChoiceDescription]::new('&GUI', 'GUI Install - A UI to choose install options.')
-$quit = [ChoiceDescription]::new('&Quit', 'Quit and restart the device.')
-$options = [ChoiceDescription[]]($zti, $gui, $quit)
-$result = $host.ui.PromptForChoice($title, $question, $options, 0)
-switch ($result) {
-    0 { 
-        # Start OSDCloud Process
-        Write-Host -ForegroundColor Cyan "Starting OSDCloud"
-        Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI
-
-        # Restart
-        Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
-        Start-Sleep -Seconds 20
-        wpeutil reboot
-    }
-    1 { 
-        # Start OSDCloudGUI
-        Start-OSDCloudGUI
-    }
-    2 { 
-        # Restart
-        Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
-        Start-Sleep -Seconds 20
-        wpeutil reboot
+function Start-DeploymentMenu {
+    $title = ''
+    $question = 'Please choose a deployment option:'
+    $zti = [ChoiceDescription]::new('&ZTI', 'Zero Touch Install - This is fully automated without any prompts.')
+    $gui = [ChoiceDescription]::new('&GUI', 'GUI Install - A UI to choose install options.')
+    $quit = [ChoiceDescription]::new('&Quit', 'Quit and restart the device.')
+    $options = [ChoiceDescription[]]($zti, $gui, $quit)
+    $result = $host.ui.PromptForChoice($title, $question, $options, 0)
+    switch ($result) {
+        0 { 
+            # Start OSDCloud Process
+            Write-Host -ForegroundColor Cyan "Starting OSDCloud"
+            Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI
+    
+            # Restart
+            Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
+            Start-Sleep -Seconds 20
+            wpeutil reboot
+        }
+        1 { 
+            # Start OSDCloudGUI
+            do {
+                Start-Sleep 1
+            } while (
+                Start-OSDCloudGUI
+            )
+            break
+        }
+        2 { 
+            # Restart
+            Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
+            Start-Sleep -Seconds 20
+            wpeutil reboot
+        }
     }
 }
+
+Start-DeploymentMenu
