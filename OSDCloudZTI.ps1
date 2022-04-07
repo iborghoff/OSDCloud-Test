c0f620aae6832fe34b09a38a75b7a9f11f60defb
Iain Borghoff <38186303+iborghoff@users.noreply.github.com>
2022-04-07 16:19:58 +0200

    Update OSDCloudZTI.ps1

--- OSDCloudZTI.ps1
+++ OSDCloudZTI.ps1
@@ -1,48 +1,34 @@
 using namespace System.Management.Automation.Host
 
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud ..."
-Start-Sleep -Seconds 5
+$scriptverion = '1.0'
+Write-Host -ForegroundColor Yellow "Script version $scriptverion"
 
-# Menu
-function Deployment-Menu {
-    $title = ''
-    $question = 'Please choose a deployment option:'
-    $zti = [ChoiceDescription]::new('&ZTI', 'Zero Touch Install - This is fully automated without any prompts.')
-    $gui = [ChoiceDescription]::new('&GUI', 'GUI Install - A UI to choose install options.')
-    $quit = [ChoiceDescription]::new('&Quit', 'Quit and restart the device.')
-    $options = [ChoiceDescription[]]($zti, $gui, $quit)
-    $result = $host.ui.PromptForChoice($title, $question, $options, 0)
-    switch ($result) {
-        0 { 
-            # Update OSD
-            Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-            Install-Module OSD -Force
+$title = ''
+$question = 'Please choose a deployment option:'
+$zti = [ChoiceDescription]::new('&ZTI', 'Zero Touch Install - This is fully automated without any prompts.')
+$gui = [ChoiceDescription]::new('&GUI', 'GUI Install - A UI to choose install options.')
+$quit = [ChoiceDescription]::new('&Quit', 'Quit and restart the device.')
+$options = [ChoiceDescription[]]($zti, $gui, $quit)
+$result = $host.ui.PromptForChoice($title, $question, $options, 0)
+switch ($result) {
+    0 { 
+        # Start OSDCloud Process
+        Write-Host -ForegroundColor Cyan "Starting OSDCloud"
+        Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI
 
-            Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-            Import-Module OSD -Force
-
-            # Start OSDCloud Process
-            Write-Host  -ForegroundColor Cyan "Starting OSDCloud"
-            $userconf = Read-Host -Prompt 'Choose deployment method:' 
-
-            switch ($userconf) {
-                UI {  }
-                ZTI { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI }
-                Default { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI }
-            }
-
-            # Restart
-            Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-            Start-Sleep -Seconds 20
-            wpeutil reboot
-        }
-        1 { 
-            Start-OSDCloudGUI
-        }
-        2 { 
-            Write-Host 'Restarting device.'
-            wpeutil reboot
-         }
+        # Restart
+        Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
+        Start-Sleep -Seconds 20
+        wpeutil reboot
+    }
+    1 { 
+        # Start OSDCloudGUI
+        Start-OSDCloudGUI
+    }
+    2 { 
+        # Restart
+        Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
+        Start-Sleep -Seconds 20
+        wpeutil reboot
     }
 }
-Deployment-Menu
