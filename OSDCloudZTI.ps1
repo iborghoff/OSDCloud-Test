af1c17ca3fbb6fa53f64eca6d8bf600d0d82cc56
Iain Borghoff <38186303+iborghoff@users.noreply.github.com>
2022-04-07 16:08:43 +0200

    Update OSDCloudZTI.ps1

--- OSDCloudZTI.ps1
+++ OSDCloudZTI.ps1
@@ -1,24 +1,48 @@
+using namespace System.Management.Automation.Host
+
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud ..."
 Start-Sleep -Seconds 5
 
-# Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
+# Menu
+function Deployment-Menu {
+    $title = ''
+    $question = 'Please choose a deployment option:'
+    $zti = [ChoiceDescription]::new('&ZTI', 'Zero Touch Install - This is fully automated without any prompts.')
+    $gui = [ChoiceDescription]::new('&GUI', 'GUI Install - A UI to choose install options.')
+    $quit = [ChoiceDescription]::new('&Quit', 'Quit and restart the device.')
+    $options = [ChoiceDescription[]]($zti, $gui, $quit)
+    $result = $host.ui.PromptForChoice($title, $question, $options, 0)
+    switch ($result) {
+        0 { 
+            # Update OSD
+            Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+            Install-Module OSD -Force
+
+            Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
+            Import-Module OSD -Force
+
+            # Start OSDCloud Process
+            Write-Host  -ForegroundColor Cyan "Starting OSDCloud"
+            $userconf = Read-Host -Prompt 'Choose deployment method:' 
+
+            switch ($userconf) {
+                UI {  }
+                ZTI { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI }
+                Default { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI }
+            }
+
+            # Restart
+            Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+            Start-Sleep -Seconds 20
+            wpeutil reboot
+        }
+        1 { 
+            Start-OSDCloudGUI
+        }
+        2 { 
+            Write-Host 'Restarting device.'
+            wpeutil reboot
+         }
+    }
 }
-
-# Update OSD
-Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-Install-Module OSD -Force
-
-Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-
-# Start OSDCloud ZTI
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI
-
-# Restart
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
+Deployment-Menu
